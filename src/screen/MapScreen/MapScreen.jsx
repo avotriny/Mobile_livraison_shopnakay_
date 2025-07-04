0bac18de5df358e02e7a8995af1982b426c66c3c
Avotriniaina <avotriniainajonatan@gmail.com>
2025-07-04 20:26:38 +0200

    Carte

---
+++ src/screen/MapScreen/MapScreen.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const MapScreen = ()=> {
+  return (
+    <View style={styles.screenContainer}>
+      <Text style={styles.screenText}>Carte</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  screenText: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+});
+export default MapScreen;
